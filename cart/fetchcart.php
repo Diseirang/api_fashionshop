47478196ba053b496dab3d55354a78eaf0adbefa
DiseirangCHHEUN <111745190+Diseirang@users.noreply.github.com>
2023-08-30 07:18:41 +0700

    edit fetch cart item

--- cart/fetchcart.php
+++ cart/fetchcart.php
@@ -1,27 +1,23 @@
 <?php
 include "../connection.php";
 
-$currentOnlineUserID=$_POST['currentOnlineUserID'];
-$sqlQuery = "SELECT * FROM cart_tbl CROSS JOIN items_tbl WHERE cart_tbl.user_id = '$currentOnlineUserID' AND cart_tbl.item_id = items_tbl.item_id";  
+$currentOnlineUserID = $_POST['currentOnlineUserID'] ?? null;
+$sqlQuery = "SELECT * FROM cart_tbl CROSS JOIN items_tbl WHERE cart_tbl.user_id = '$currentOnlineUserID' AND cart_tbl.item_id = items_tbl.item_id";
 
-$resultOfQuery = $conn-> query($sqlQuery);
+$resultOfQuery = $conn->query($sqlQuery);
 
-if($resultOfQuery->num_rows>0) 
-{
-    $cartRecord =array();
-    while($rowFound = $resultOfQuery-> fetch_assoc())
-    {
-        $cartRecord[]= $rowFound;
+if ($resultOfQuery->num_rows > 0) {
+    $cartRecord = array();
+    while ($rowFound = $resultOfQuery->fetch_assoc()) {
+        $cartRecord[] = $rowFound;
     }
     echo json_encode(
         array(
-            'success'=>true,
-            'currentUserCartData'=> $cartRecord,
+            'success' => true,
+            'currentUserCartData' => $cartRecord,
         )
     );
-}
-else
-{
-    echo json_encode(array('success'=>false));
+} else {
+    echo json_encode(array('success' => false));
 
 }
